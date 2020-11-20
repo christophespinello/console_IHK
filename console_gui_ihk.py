db08df45e2bf79dbf2772f840b3cea2d117d96cb
spine <spine@spine-ROG-Strix-G531GU>
2020-11-20 19:06:45 +0100

    initial commit

---
+++ console_gui_ihk.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+from ihk_serial import SerialThread
+import threading
+import queue
+import json
+import time
+
+JSON_FILENAME = "./ihk_tests.json"
+
+DEBUG = 0
+
+class Console_GUI_IHK(tk.Tk):
+    def __init__(self) :
+        tk.Tk.__init__(self)
+        
+#        self.root = tk.Tk() # Création de la fenêtre racine
+        self.title('ConsoleGUI')
+        self.geometry("720x480")
+
+        self.json_filename = JSON_FILENAME
+    
+        with open(self.json_filename, 'r') as f:
+            self.macros = json.load(f)      
+            
+#================================================================
+# LabelFrame Actions        
+#================================================================
+        self.l_actions = tk.LabelFrame(self, text="Actions")
+        self.l_actions.pack(fill=tk.BOTH, expand=tk.YES)
+             
+# Vertical (y) Scroll Bar
+        self.scroll_actions = tk.Scrollbar(self.l_actions)
+        self.scroll_actions.pack(side=tk.RIGHT, fill=tk.Y)
+        
+# Liste des macros        
+        self.listbox = tk.Listbox(self.l_actions)
+        self.listbox.pack(fill=tk.X)
+
+        for macro in self.macros :
+            self.listbox.insert(tk.END,macro['command'] + "(" + macro['description'] + ")")
+
+#================================================================
+# LabelFrame Activite        
+#================================================================
+        self.l_activite = tk.LabelFrame(self, text="Activite", padx=10, pady=10)
+        self.l_activite.pack(fill=tk.X, expand=tk.YES)
+
+#================================================================
+# LabelFrame Envoi        
+#================================================================
+        self.l_envoi = tk.LabelFrame(self.l_activite, text="Envoi", padx=10, pady=10)
+        self.l_envoi.pack(fill=tk.X, expand=tk.YES)
+        
+        self.l_envoi.grid_columnconfigure(0, weight=10)
+        self.l_envoi.grid_columnconfigure(1, weight=1)
+
+        #Partie envoi
+        self.entry = tk.Entry(self.l_envoi)
+        self.entry.grid(row=0, column=0,sticky=tk.W+tk.E)
+        #entry.pack(fill=X, expand=YES)
+        #entry.pack(fill = BOTH,expand=YES)
+        
+        self.button_send = tk.Button(self.l_envoi,text="Send",command=self.send)
+        self.button_send.grid(row=0, column=1)
+        self.bind('<Return>',self.event_key_return)
+        #button_send.pack()
+        
+#================================================================
+# LabelFrame Dialogue        
+#================================================================
+        self.l_traffic = tk.LabelFrame(self.l_activite, text="Dialogue", padx=10, pady=10)
+        self.l_traffic.pack(fill=tk.X, expand=tk.YES)
+        
+        # Vertical (y) Scroll Bar
+        self.scroll = tk.Scrollbar(self.l_traffic)
+        self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
+        
+        self.textzone = tk.Text(self.l_traffic,height=10, yscrollcommand=self.scroll.set)
+        self.textzone.pack(fill = tk.BOTH,expand=tk.YES)
+        self.textzone.tag_config('send', foreground="blue")
+        self.textzone.tag_config('receive', foreground="green")
+        self.textzone.tag_config('comment', foreground="black")
+        self.textzone.tag_config('error', foreground="red")
+        
+#================================================================
+# LabelFrame Commande        
+#================================================================
+        self.l_command = tk.LabelFrame(self, text="Commands", padx=10, pady=10)
+        self.l_command.pack(fill=tk.X, expand=tk.YES)
+        self.l_command.grid_columnconfigure(0, weight=1)
+        self.l_command.grid_columnconfigure(1, weight=1)
+        self.l_command.grid_columnconfigure(2, weight=1)
+        
+        self.button_options = tk.Button(self.l_command,text='OPTIONS')
+        self.button_options.grid(row=0, column=0,sticky=tk.W+tk.E,padx=20)
+        
+        self.button_clear = tk.Button(self.l_command,text='CLEAR')
+        self.button_clear.grid(row=0, column=1,sticky=tk.W+tk.E,padx=20)
+
+        self.button_quit = tk.Button(self.l_command,text='QUIT')
+        self.button_quit.grid(row=0, column=2,sticky=tk.W+tk.E,padx=20)
+ 
+        self.queue = queue.Queue()
+        
+        self.readThread = SerialThread(self.queue)
+        self.readThread.start()
+        self.processConsole()  
+            
+    def processConsole(self) :
+        while self.queue.qsize():
+            try:
+                new=self.queue.get()
+                self.textzone.insert(tk.END,new,"receive")
+                self.textzone.see(tk.END)
+            except queue.Empty:
+                pass
+        self.after(100, self.processConsole)
+      
+    def send(self):
+        if self.entry.get() == "":
+            if len(self.listbox.curselection()) == 0 :
+                self.textzone.insert(tk.END,"No command to send" + "\n","error")
+            else :
+                self.send_macro(self.listbox.curselection()[0])
+        else :
+            self.readThread.send_frame(self.entry.get())
+            self.textzone.insert(tk.END,self.entry.get() + "\n","send")
+        self.entry.delete(0, tk.END)
+        
+    def send_macro(self,index):
+        i = 0
+        for macro in self.macros :
+            if (i == index) :
+                self.textzone.insert(tk.END,"Macro " + macro['command'] + "\n","comment")
+                for action in macro['action'] :
+                    if (action[:5] == "SLEEP") :
+                        time.sleep(int(action[6:]))
+                        self.textzone.insert(tk.END,action + "\n","comment")
+                    else :                                                
+                        self.readThread.send_frame(action)
+                        self.textzone.insert(tk.END,action + "\n","send")
+            i = i+1
+        
+        
+            
+    def event_key_return(self,event):
+        self.send()
+        
+if __name__ == '__main__':
+    app=Console_GUI_IHK()
+    app.mainloop() 
+    # Lancement de la boucle principale
---
+++ ihk_serial.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python3
+
+from utils import load_config, save_config
+from serial_ports import serial_ports
+import serial
+import threading
+import queue 
+
+CONFIG_FILENAME = './config.yml'
+
+DEBUG_NO_COMM = True
+
+ERR_IHK_SERIAL_OK = 0
+ERR_IHK_SERIAL_NO_FRAME = -1
+
+class SerialThread(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+        self.config = load_config(CONFIG_FILENAME)
+        self.port = self.config['comport']
+        self.debit = self.config['baudrate']
+        self.debug = False
+        if (self.config['debug']):
+            self.debug = True
+        if not DEBUG_NO_COMM :
+            self.serialPort = serial.Serial(self.port,self.debit)
+        
+    def save_configuration(self) :
+        self.config['debug'] = False
+        if (self.debug) :
+            self.config['debug'] = True
+        save_config(self.config, CONFIG_FILENAME)
+        return("Configuration saved")
+        
+
+    def run(self):
+#        self.serialPort.write(str.encode('help\n'))
+#        time.sleep(0.2)
+        while True:
+            if not DEBUG_NO_COMM :
+                if self.serialPort.inWaiting():
+    #                text = self.serialPort.readline(self.serialPort.inWaiting())
+                    try:
+                        text = self.serialPort.readline(self.serialPort.inWaiting()).decode("utf-8")
+                    except :
+                        print("Error reading Line\n")
+                        continue    
+                    text = text.replace("\r","\r\n")
+                    self.queue.put(text)
+                
+    def send_frame(self, s):
+        if not DEBUG_NO_COMM :
+            self.serialPort.write(str.encode(s + "\n\r"))
+        
+class IHK_Serial(object):
+
+    def __init__(self) :
+        self.config = load_config(CONFIG_FILENAME)
+        self.port = self.config['comport']
+        self.debit = self.config['baudrate']
+        self.debug = False
+        if (self.config['debug']):
+            self.debug = True
+
+    def save_configuration(self) :
+        self.config['debug'] = False
+        if (self.debug) :
+            self.config['debug'] = True
+        save_config(self.config, CONFIG_FILENAME)
+        print(bcolors.WARNING+"Configuration saved"+bcolors.INFO)
+            
+    def configure(self):
+        print(bcolors.WARNING)
+        print(".........................................")
+        print("Serial port configuration")
+        print(".........................................")
+        print(bcolors.ENDC)
+        print("List of serial ports ...")
+        results = serial_ports()
+        config = load_config(CONFIG_FILENAME)
+        print("[-] : Do not change (" + config['comport'] + " selected) [Default]")
+        for i in (range(1, len(results) + 1)) :
+            print("[" + str(i) + '] : ' + results[i - 1])
+        try:
+            selected = input(bcolors.ACTION+"Type the Uart port to use and press <Enter> : "+bcolors.ENDC)
+        except ValueError:
+            pass
+        if (selected != "0") and (selected != ""):
+            self.port = results[int(selected) - 1]
+            self.config['comport'] = self.port
+            save_config(self.config, CONFIG_FILENAME)
+            print("Configuration saved")
+            
+    def open(self):        
+        self.serialPort = serial.Serial(self.port, self.debit, timeout=0)
+        
+    def read_frame(self):
+        ret = dict([('err', ERR_IHK_SERIAL_NO_FRAME), ('str', "")])
+        
+        ret['err'] = ERR_IHK_SERIAL_NO_FRAME
+        line = self.serialPort.read(128)
+        for i in range(len(line)) :
+            ret['str'] = ret['str'] + chr(line[i])
+
+    def write_frame(self, str):
+        self.serialPort.write(str.encode(str + "\n"))
+            
+    def send_frame(self, str, timeout=0.1, display_received_data = False):
+        str_serial = []
+        self.serialPort = serial.Serial(self.port, self.debit, timeout=timeout)
+
+        self.serialPort.write((str + '\r\n').encode('utf-8'))
+        
+        if (self.debug) :
+            print(">> Send : " + str)
+
+        err = self.receive_frame()
+        if (err['str'] != "") :
+            if (self.debug) :
+                print("<< Receive : " + err['str'])
+            elif (display_received_data) :
+                print(err['str'])
+        self.serialPort.close()
+        return err
+        
+    def receive_frame(self) :
+        ret = dict([('err', ERR_IHK_SERIAL_NO_FRAME), ('str', "")])
+
+        while (True) :
+            ret['err'] = ERR_IHK_SERIAL_NO_FRAME
+            line = self.serialPort.read(128)
+            if (line == b'') :
+                return ret
+            ret['err'] = ERR_IHK_SERIAL_OK
+            for i in range(len(line)) :
+                ret['str'] = ret['str'] + chr(line[i])
+#            return(ret)
+    
+    def receive_alive_frame(self) :
+        ret = self.send_frame(str = "version",display_received_data = True)
+
+    
+    def connect(self) :
+        self.m_compteurTickFrames = 0
+        print("Connect to the product ...")
+        
+        self.receive_alive_frame()
+        
+
+
+
+    
