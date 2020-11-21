375c4617dd2ca03a2f1d16f44cddbec886c16366
spine <spine@spine-ROG-Strix-G531GU>
2020-11-21 10:21:25 +0100

    Changement disposition fenetre

--- console_gui_ihk.py
+++ console_gui_ihk.py
@@ -18,7 +18,7 @@ class Console_GUI_IHK(tk.Tk):
 #        self.root = tk.Tk() # Création de la fenêtre racine
         self.title('ConsoleGUI')
 #        self.attributes('-fullscreen', True)
-        self.geometry("720x480")
+#        self.geometry("720x480")
 
         self.json_filename = JSON_FILENAME
     
@@ -26,23 +26,42 @@ class Console_GUI_IHK(tk.Tk):
             self.macros = json.load(f)      
             
 #================================================================
-# LabelFrame Actions        
+# Construction fenetres principale        
 #================================================================
-        self.l_actions = tk.LabelFrame(self, text="Actions")
-        self.l_actions.grid(row=0, column=0,sticky=tk.W+tk.E)
-#        self.l_actions.pack(fill=tk.BOTH)
-             
+        self.l_dialogue = tk.LabelFrame(self, text="Dialog")
+        self.l_dialogue.grid(row=0, column=0,sticky=tk.W+tk.E+tk.N+tk.S)
+
+#        self.l_actions = tk.LabelFrame(self, text="Actions")
+#        self.l_actions.grid(row=0, column=1,sticky=tk.W+tk.E+tk.N+tk.S)
+
+        self.l_command = tk.LabelFrame(self, text="Commands", padx=10, pady=10)
+        self.l_command.grid(row=0, column=1,sticky=tk.W+tk.E+tk.N+tk.S)
+#        self.l_command.pack(fill=tk.BOTH, expand=tk.YES)
+
+#================================================================
+# LabelFrame dans dialogue        
+#================================================================
+
+#================================================================
+# LabelFrame Macros        
+#================================================================
+        self.l_macros = tk.LabelFrame(self.l_dialogue, text="Macros",padx=2, pady=2)
+        self.l_macros.grid(row=1, column=0,sticky=tk.W+tk.E)
+
 # Vertical (y) Scroll Bar
-        self.scroll_actions = tk.Scrollbar(self.l_actions)
-        self.scroll_actions.pack(side=tk.RIGHT, fill=tk.Y)
+        self.scroll_macros = tk.Scrollbar(self.l_macros)
+        self.scroll_macros.pack(side=tk.RIGHT, fill=tk.Y)
         
 # Liste des macros        
-        self.listbox = tk.Listbox(self.l_actions,height=5)
+        self.listbox = tk.Listbox(self.l_macros,yscrollcommand=self.scroll_macros,height=5)
+#        self.listbox.grid(row=1, column=0,sticky=tk.W+tk.E)
         self.listbox.pack(fill=tk.X)
 
         for macro in self.macros :
             self.listbox.insert(tk.END,macro['command'] + "(" + macro['description'] + ")")
 
+        self.scroll_macros.config(command = self.listbox.yview)
+        
 #================================================================
 # LabelFrame Activite        
 #================================================================
@@ -53,63 +72,61 @@ class Console_GUI_IHK(tk.Tk):
 #================================================================
 # LabelFrame Envoi        
 #================================================================
-        self.l_envoi = tk.LabelFrame(self, text="Envoi", padx=10, pady=10)
-        self.l_envoi.grid(row=1, column=0,sticky=tk.W+tk.E)
+        self.l_envoi = tk.LabelFrame(self.l_dialogue, text="Envoi", padx=2, pady=2)
+        self.l_envoi.grid(row=2, column=0,sticky=tk.W+tk.E)
 #        self.l_envoi.pack(fill=tk.X, expand=tk.YES)
         
-        self.l_envoi.grid_columnconfigure(0, weight=10)
-        self.l_envoi.grid_columnconfigure(1, weight=1)
-
         #Partie envoi
         self.entry = tk.Entry(self.l_envoi)
-        self.entry.grid(row=0, column=0,sticky=tk.W+tk.E)
+        self.entry.pack(fill=tk.BOTH)
         #entry.pack(fill=X, expand=YES)
         #entry.pack(fill = BOTH,expand=YES)
-        
-        self.button_send = tk.Button(self.l_envoi,text="Send",command=self.send)
-        self.button_send.grid(row=0, column=1)
-        self.bind('<Return>',self.event_key_return)
-        #button_send.pack()
         
 #================================================================
 # LabelFrame Dialogue        
 #================================================================
 #        self.l_traffic = tk.LabelFrame(self.l_activite, text="Dialogue", padx=10, pady=10)
-        self.l_traffic = tk.LabelFrame(self, text="Dialogue", padx=10, pady=10)
-        self.l_traffic.grid(row=2, column=0)
+        self.l_traffic = tk.LabelFrame(self.l_dialogue, text="Communication receive", padx=2, pady=2)
+        self.l_traffic.grid(row=3, column=0)
 #        self.l_traffic.pack(fill=tk.X, expand=tk.YES)
         
         # Vertical (y) Scroll Bar
-        self.scroll = tk.Scrollbar(self.l_traffic)
-        self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
-        
-        self.textzone = tk.Text(self.l_traffic,height=10, yscrollcommand=self.scroll.set, state=tk.NORMAL)
+        self.scroll_textzone = tk.Scrollbar(self.l_traffic)
+        self.scroll_textzone.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.textzone = tk.Text(self.l_traffic,height=5, yscrollcommand=self.scroll_textzone.set, state=tk.NORMAL)
+#        self.textzone = tk.Text(self.l_traffic,height=10, state=tk.NORMAL)
         self.textzone.pack(fill = tk.BOTH,expand=tk.YES)
+
         self.textzone.tag_config('send', foreground="blue")
         self.textzone.tag_config('receive', foreground="green")
         self.textzone.tag_config('comment', foreground="black")
         self.textzone.tag_config('error', foreground="red")
         
+        self.scroll_textzone.config(command = self.textzone.yview)
+        
 #================================================================
 # LabelFrame Commande        
 #================================================================
-        self.l_command = tk.LabelFrame(self, text="Commands", padx=10, pady=10)
-        self.l_command.grid(row=3, column=0,sticky=tk.W+tk.E)
-#        self.l_command.pack(fill=tk.BOTH, expand=tk.YES)
 
-        self.l_command.grid_columnconfigure(0, weight=1)
-        self.l_command.grid_columnconfigure(1, weight=1)
-        self.l_command.grid_columnconfigure(2, weight=1)
-        self.l_command.grid_columnconfigure(3, weight=1)
+        self.l_command.grid_rowconfigure(0, weight=1)
+        self.l_command.grid_rowconfigure(1, weight=1)
+        self.l_command.grid_rowconfigure(2, weight=1)
+        self.l_command.grid_rowconfigure(3, weight=1)
         
+        self.button_send = tk.Button(self.l_command,text="SEND",command=self.send)
+        self.button_send.grid(row=0, column=0,sticky=tk.W+tk.E,padx=20)
+        self.bind('<Return>',self.event_key_return)
+        #button_send.pack()
+
         self.button_options = tk.Button(self.l_command,text='OPTIONS')
-        self.button_options.grid(row=0, column=0,sticky=tk.W+tk.E,padx=20)
+        self.button_options.grid(row=1, column=0,sticky=tk.W+tk.E,padx=20)
         
         self.button_clear = tk.Button(self.l_command,text='CLEAR')
-        self.button_clear.grid(row=0, column=1,sticky=tk.W+tk.E,padx=20)
+        self.button_clear.grid(row=2, column=0,sticky=tk.W+tk.E,padx=20)
 
         self.button_quit = tk.Button(self.l_command,text='QUIT',command=self.quit)
-        self.button_quit.grid(row=0, column=2,sticky=tk.W+tk.E,padx=20)
+        self.button_quit.grid(row=3, column=0,sticky=tk.W+tk.E,padx=20)
  
         self.queue = queue.Queue()
         self.queueSend = queue.Queue()
